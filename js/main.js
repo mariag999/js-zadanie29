3d48af53ef017de360945abeb24b7f0151b7cb2a
mgogacz <maria.gogacz23@gmail.com>
2024-04-24 18:34:37 +0200

    utworzenie pliku

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let firstNumber = "25"
+let secondNumber = "50"
+
+const mnozenieLiczb = function (liczba1 = 0, liczba2 = 0) {
+    let wynik = liczba1 * liczba2;
+
+    return wynik;
+}
+
+let wynikMnozenia = 0;
+
+wynikMnozenia = mnozenieLiczb(25, 50);
+
+console.log(wynikMnozenia);
